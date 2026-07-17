76e48eed5225b78c33fd64b586e025b9ce373e97
rsdadada <rsddadada@outlook.com>
2026-07-18 01:46:41 +0800

    Restore generic routing collector

--- common/src/main/java/com/xtracr/realcamera/renderer/RoutingSubmitCollector.java
+++ common/src/main/java/com/xtracr/realcamera/renderer/RoutingSubmitCollector.java
@@ -25,25 +25,14 @@ import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
 import java.util.List;
-import java.util.function.BiFunction;
 
 public final class RoutingSubmitCollector implements SubmitNodeCollector {
-    private static final BiFunction<RenderType, CustomGeometryRenderer, CustomGeometryRenderer> IDENTITY_DECORATOR = (_, renderer) -> renderer;
     private final SubmitNodeCollector defaultCollector;
     private final SubmitNodeCollector modelCollector;
-    private final BiFunction<RenderType, CustomGeometryRenderer, CustomGeometryRenderer> customGeometryDecorator;
 
     public RoutingSubmitCollector(SubmitNodeCollector defaultCollector, SubmitNodeCollector modelCollector) {
-        this(defaultCollector, modelCollector, IDENTITY_DECORATOR);
-    }
-
-    public RoutingSubmitCollector(
-            SubmitNodeCollector defaultCollector,
-            SubmitNodeCollector modelCollector,
-            BiFunction<RenderType, CustomGeometryRenderer, CustomGeometryRenderer> customGeometryDecorator) {
         this.defaultCollector = defaultCollector;
         this.modelCollector = modelCollector;
-        this.customGeometryDecorator = customGeometryDecorator;
     }
 
     @Override
@@ -108,7 +97,7 @@ public final class RoutingSubmitCollector implements SubmitNodeCollector {
 
     @Override
     public void submitCustomGeometry(@NonNull PoseStack poseStack, @NonNull RenderType renderType, @NonNull CustomGeometryRenderer customGeometryRenderer) {
-        modelCollector.submitCustomGeometry(poseStack, renderType, customGeometryDecorator.apply(renderType, customGeometryRenderer));
+        modelCollector.submitCustomGeometry(poseStack, renderType, customGeometryRenderer);
     }
 
     @Override
@@ -182,7 +171,7 @@ public final class RoutingSubmitCollector implements SubmitNodeCollector {
 
         @Override
         public void submitCustomGeometry(@NonNull PoseStack poseStack, @NonNull RenderType renderType, @NonNull CustomGeometryRenderer customGeometryRenderer) {
-            modelDelegate.submitCustomGeometry(poseStack, renderType, customGeometryDecorator.apply(renderType, customGeometryRenderer));
+            modelDelegate.submitCustomGeometry(poseStack, renderType, customGeometryRenderer);
         }
 
         @Override
